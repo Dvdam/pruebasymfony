ac9d03781328f4859517c7c4a323fb2aa2d0fdba
Dvdam <rdvdam@gmail.com>
2019-08-14 02:34:40 -0300

    add composer asset

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PostController
+class PostController extends AbstractController
 {
     /**
      * @Route()
@@ -12,5 +13,16 @@ class PostController
     public function home()
     {
         return new Response('Hello tech yogi friends!!');
+    }
+
+    /**
+     * @Route("/blog/{post}")
+     */
+    public function showPost($post)
+    {
+        // return new Response(sprintf('Future page to show: "%s"', $post));
+        return $this->render('posts/post.html.twig', [
+            'title' => ucwords(str_replace('-','',$post))
+        ]);
     }
 }
